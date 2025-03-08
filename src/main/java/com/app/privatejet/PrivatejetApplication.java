99eb1290d928fe4984913a3deffadbb95c3275f4
sofia <sofiaaristizabal23@gmail.com>
2025-03-08 11:59:38 -0500

    first commit

---
+++ src/main/java/com/app/privatejet/PrivatejetApplication.java
@@ -0,0 +1,13 @@
+package com.app.privatejet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatejetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivatejetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/privatejet/PrivatejetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.privatejet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatejetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
